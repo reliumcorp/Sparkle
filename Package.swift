c2a6069bbeadf3163cd60fd43c2837ab965b851a
Eric Shapiro <shapiro@relium.com>
2022-01-04 10:01:10 -0500

    2.0.3

--- Package.swift
+++ Package.swift
@@ -2,9 +2,9 @@
 import PackageDescription
 
 // Version is technically not required here, SPM doesn't check
-let version = "2.0.2"
+let version = "2.0.3"
 // Tag is required to point towards the right asset. SPM requires the tag to follow semantic versioning to be able to resolve it.
-let tag = "2.0.2"
+let tag = "2.0.3"
 let checksum = "095e1c65484486350497cedc9cc297b9e89394af55cc4186e660f547ad262c7c"
 let url = "https://github.com/reliumcorp/Sparkle/releases/download/\(tag)/Sparkle-for-Swift-Package-Manager.zip"
 
